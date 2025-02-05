6ccc1e2cb0839849038775237222370c69aed4d8
Vasiliy <ballov_v@mail.ru>
2025-02-05 12:16:57 +0300

    added WS urls

--- src/Components/hoc/WebSocket/WebSocketListener.js
+++ src/Components/hoc/WebSocket/WebSocketListener.js
@@ -1,7 +1,12 @@
 import { useWebSocket } from './useWebSocket';
 const token = localStorage.getItem('token');
 
+const wsPath =
+  process.env.NODE_ENV === 'development'
+    ? `ws://${process.env.REACT_APP_DEV_API_URL.replace(/^http:\/\//, '')}/ws/notifications/?token=${token}`
+    : `ws://${process.env.REACT_APP_PROD_API_URL.replace(/^http:\/\//, '')}/ws/notifications/?token=${token}`;
+
 export const WebSocketListener = () => {
-  useWebSocket(`ws://192.168.0.233:8000/ws/notifications/?token=${token}`);
+  useWebSocket(wsPath);
   return null;
 };
--- src/Components/hoc/WebSocket/WebSocketMenuListener.js
+++ src/Components/hoc/WebSocket/WebSocketMenuListener.js
@@ -1,7 +1,12 @@
 import { useWebSocket } from './useWebSocketSideBar';
 const token = localStorage.getItem('token');
 
+const wsMenuPath =
+  process.env.NODE_ENV === 'development'
+    ? `ws://${process.env.REACT_APP_DEV_API_URL.replace(/^http:\/\//, '')}/ws/doc-sidebar/?token=${token}`
+    : `ws://${process.env.REACT_APP_PROD_API_URL.replace(/^http:\/\//, '')}/ws/doc-sidebar/?token=${token}`;
+
 export const WebSocketMenuListener = () => {
-  useWebSocket(`ws://192.168.0.233:8000/ws/doc-sidebar/?token=${token}`);
+  useWebSocket(wsMenuPath);
   return null;
 };
